db908c033b9d5516648b65c0a81d36c7f677a286
ComteUnderscoreL <135239050+ComteUnderscoreL@users.noreply.github.com>
2026-08-18 14:04:46 +0200

    Update globe.js

--- js/globe.js
+++ js/globe.js
@@ -293,7 +293,7 @@ async function load() {
   landData = topojson.feature(world, world.objects.land);
 
   for (const item of config.regions) {
-    if (item.visible === false) continue;
+    if (item.visible !== true) continue;
     let geojson = await d3.json(item.geojson);
 
     if (item.needsReverse) {
